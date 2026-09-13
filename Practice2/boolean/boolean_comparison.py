4e8e58c931c472d0b8bb630df2999bf12f1b2f6e
zinullinn <nrsltanzinullin.com>
2026-09-13 17:26:14 +0500

    Add Practice2 Boolean examples

---
+++ Practice2/boolean/boolean_comparison.py
@@ -0,0 +1,8 @@
+first_number = 12
+second_number = 7
+
+print(first_number > second_number)
+print(first_number < second_number)
+print(first_number == second_number)
+print(first_number != second_number)
+print(first_number >= 12)
---
+++ Practice2/boolean/boolean_operators.py
@@ -0,0 +1,9 @@
+age = 19
+has_id = True
+is_member = False
+
+print(age >= 18 and has_id)
+print(age < 18 or is_member)
+print(not is_member)
+print(age >= 18 and not is_member)
+print(has_id or is_member)
